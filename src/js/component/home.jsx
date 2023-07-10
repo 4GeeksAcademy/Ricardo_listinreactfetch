85b86f3053f98cccf594e4c515e11c4ca64d6872
kommercialkings <122939782+kommercialkings@users.noreply.github.com>
2023-07-10 22:47:56 +0000

    listinreactusingfetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  // Fetch the initial todos
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  // Function to fetch todos
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch('/api/todos'); // Replace with your backend API endpoint
+      const data = await response.json();
+      setTodos(data);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  };
+
+  // Function to add a new todo
+  const addTodo = async () => {
+    try {
+      const response = await fetch('/api/todos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title: newTodo }) // Assuming your backend expects a 'title' field
+      });
+
+      if (response.ok) {
+        fetchTodos(); // Fetch updated todos after adding a new one
+        setNewTodo(''); // Clear the input field
+      } else {
+        console.error('Error adding todo:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error adding todo:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li> // Assuming each todo object has a unique 'id' field
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={event => setNewTodo(event.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+    </div>
+  );
 };
 
-export default Home;
+export default TodoList;
